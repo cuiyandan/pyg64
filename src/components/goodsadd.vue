4b7b79ad238581d13bc85fa7c32147683cc8cdb8
daoge <2197191085@qq.com>
2019-02-20 23:17:02 +0800

    商品参数+商品属性+图片上传

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -14,7 +14,7 @@
 
     <!-- 标签页 -->
     <el-form class="form" label-position="top" label-width="80px" :model="form">
-      <el-tabs v-model="active" tab-position="left">
+      <el-tabs @tab-click="changeTab()" v-model="active" tab-position="left">
         <el-tab-pane name="1" label="基本信息">
           <!-- 1 -->
           <el-form-item label="商品名称">
@@ -32,6 +32,7 @@
           <el-form-item label="商品分类">
             <!-- 表单元素  级联选择器-->
             <el-cascader
+              clearable
               expand-trigger="hover"
               :options="options"
               :props="defaultProp"
@@ -40,9 +41,34 @@
             ></el-cascader>
           </el-form-item>
         </el-tab-pane>
-        <el-tab-pane name="2" label="商品参数">商品参数</el-tab-pane>
-        <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
-        <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
+
+        <el-tab-pane name="2" label="商品参数">
+          <el-form-item :label="item1.attr_name " v-for="(item1) in arrDy" :key="item1.attr_id">
+            <el-checkbox-group v-model="item1.attr_vals">
+              <el-checkbox border :label="item2" v-for="(item2,i) in item1.attr_vals" :key="i"></el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+        </el-tab-pane>
+
+        <el-tab-pane name="3" label="商品属性">
+          <el-form-item :label="item.attr_name" v-for="(item) in arrStatic" :key="item.attr_id">
+            <el-input v-model="item.attr_vals"></el-input>
+          </el-form-item>
+        </el-tab-pane>
+
+        <el-tab-pane name="4" label="商品图片">
+          <el-form-item>
+            <el-upload
+              action="http://localhost:8888/api/private/v1/upload"
+              :headers="headers"
+              :on-remove="handleRemove"
+              :on-success="handleSuccess"
+              list-type="picture"
+            >
+              <el-button size="small" type="primary">点击上传</el-button>
+            </el-upload>
+          </el-form-item>
+        </el-tab-pane>
         <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
       </el-tabs>
     </el-form>
@@ -67,11 +93,18 @@ export default {
       },
       //   级联使用的数据
       options: [],
-      selectedOptions: [],
+      selectedOptions: [1, 3, 6],
       defaultProp: {
-        label: "",
-        value: "",
-        children: ""
+        label: "cat_name",
+        value: "cat_id"
+      },
+
+      //动态参数数组
+      arrDy: [],
+      // 静态参数数组
+      arrStatic: [],
+      headers: {
+        Authorization: localStorage.getItem("token")
       }
     };
   },
@@ -79,10 +112,72 @@ export default {
     this.getGoodsCate();
   },
   methods: {
+    // 图片上传相关方法
+    handleRemove(file, fileList) {
+      // console.log("remove---");
+      console.log(file);
+    },
+    handleSuccess(response, file, fileList) {
+      // console.log("chenggong");
+      console.log(response);
+    },
+    // 点击tab触发
+    async changeTab() {
+      // 若果点了第二个tab
+      if (this.active === "2" || this.active === "3") {
+        if (this.selectedOptions.length !== 3) {
+          // 提示
+          this.$message.error("请先选择三级分类");
+          return;
+        }
+        // 获取静态数据
+        if (this.active === "3") {
+          const res = await this.$http.get(
+            `categories/${this.selectedOptions[2]}/attributes?sel=only`
+          );
+          // console.log(res);
+          const {
+            meta: { msg, status },
+            data
+          } = res.data;
+          if (status === 200) {
+            this.arrStatic = data;
+            console.log(this.arrStatic);
+          }
+        }
+
+        //获取动态参数数据
+        const res = await this.$http.get(
+          `categories/${this.selectedOptions[2]}/attributes?sel=many`
+        );
+        // console.log(res);
+        const {
+          meta: { msg, status },
+          data
+        } = res.data;
+        if (status === 200) {
+          this.arrDy = data;
+          // console.log("----");
+          // console.log(this.arrDy);
+          this.arrDy.forEach(item => {
+            // 空字符串处理
+            // if (item.attr_vals.length === 0) {
+            //   item.attr_vals = [];
+            // } else {
+            //   item.attr_vals = item.attr_vals.trim().split(",");
+            // }
+            item.attr_vals =
+              item.attr_vals.trim().length === 0
+                ? []
+                : item.attr_vals.trim().split(",");
+          });
+        }
+      }
+    },
     //   获取三级商品分类数据
     async getGoodsCate() {
       const res = await this.$http.get(`categories?type=3`);
-      console.log(res);
+      // console.log(res);
       const {
         meta: { msg, status },
         data
