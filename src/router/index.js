6e9d38982e4849974c9a45c5013478c83c83e75b
daoge <2197191085@qq.com>
2019-02-21 21:08:10 +0800

    分类参数

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Rights from '../components/rights.vue'
 import Roles from '../components/roles.vue'
 import Goodslist from '../components/goodslist.vue'
 import Goodsadd from '../components/goodsadd.vue'
+import Cateparams from '../components/cateparams.vue'
 
 Vue.use(Router)
 
@@ -40,6 +41,10 @@ const router = new Router({
       name: 'goodsadd',
       path: '/goodsadd',
       component: Goodsadd
+    }, {
+      name: 'params',
+      path: '/params',
+      component: Cateparams
     }
     ]
   }, {
